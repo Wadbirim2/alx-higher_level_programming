323046dbef2f52d86bfb918a31e91bba18bcc32b
Amir Abbakar <bainkwe@outlook.com>
2023-05-12 10:28:11 -0700

    Replace element

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+# 2-replace_in_list.py
+# Brennan D Baraban <375@holbertonschool.com>
+
+
 def replace_in_list(my_list, idx, element):
-    replace_in_list = __import__('2-replace_in_list').replace_in_list
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_element = 9
-    new_list = replace_in_list(my_list, idx, new_element)
-    print(new_list)
-print(my_list)
+    """Replace an element of a list at a specific position."""
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
