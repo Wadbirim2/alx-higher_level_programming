38a572337ac33ca063791aff795748476414300e
Amir Abbakar <bainkwe@outlook.com>
2023-05-12 11:30:05 -0700

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    max_integer = __import__('9-max_integer').max_integer
-    my_list = [1, 90, 2, 13, 34, 5, -13, 3]
-    max_value = max_integer(my_list)
-    print("Max: {}".format(max_value))
+# 9-max_integer.py
+# Brennan D Baraban <375@holbertonschool.com>
 
+
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+
+    return (big)
